7e3edbcbac649793c2bba13725233b60522bf755
Andreas Fruth <andreas.fruth@gmail.com>
2016-10-22 17:17:55 +0300

    Added octonode

--- server/main.js
+++ server/main.js
@@ -1,5 +1,34 @@
 import { Meteor } from 'meteor/meteor';
 
+import github from 'octonode';
+
+
+
+const client = github.client(Meteor.settings.github.token);
+const ghteam = client.team(Meteor.settings.github.teamid);
+
 Meteor.startup(() => {
-  // code to run on server at startup
+  ghteam.repos(function(e,r){
+    if(!e) {
+      r.forEach((item)=>{
+        let ghrepo = client.repo(item.full_name);
+
+        ghrepo.commits((er,rez) => {
+          rez.forEach((item) => {
+            console.log(item.sha)
+            ghrepo.commit(item.sha, (e,ror) => {
+              console.log(ror);
+            });
+          });
+        });
+      });
+    }
+  });
+});
+
+
+Meteor.methods({
+  getGithubData: function() {
+    console.log(client.me())
+  }
 });
