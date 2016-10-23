1ab1491d428d06ed3ac67cd4311e542988ed0f30
dumconstantin <dum.constantin@gmail.com>
2016-10-23 15:29:32 +0300

    Add logos

--- client/main.js
+++ client/main.js
@@ -16,6 +16,26 @@ Template.hello.onCreated(function helloOnCreated() {
   this.subscribe('allBranches');
 });
 
+let logos = {
+  Twig: 'http://jacob-walker.com/twig-talk/presentation/img/logo-twig.png',
+  JSON: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/JSON_vector_logo.svg/2000px-JSON_vector_logo.svg.png',
+  Markdown: 'http://kirkstrobeck.github.io/whatismarkdown.com/img/markdown.png',
+  JavaScript: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/480px-Unofficial_JavaScript_logo_2.svg.png',
+  HTML: 'https://www.w3.org/html/logo/downloads/HTML5_Logo_512.png',
+  Ruby: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Ruby_logo.svg/1000px-Ruby_logo.svg.png',
+  YAML: 'http://freevector.co/wp-content/uploads/2013/11/56156-yml-document-black-interface-symbol-200x200.png',
+  CSS: 'http://wpguru.co.uk/wp-content/uploads/2013/09/CSS-Logo-214x300.png',
+  TypeScript: 'https://raw.githubusercontent.com/remojansen/logo.ts/master/ts.png',
+  SASS: 'http://sass-lang.com/assets/img/styleguide/color-1c4aab2b.png',
+  Pug: 'https://pbs.twimg.com/media/Ckaxc1pWEAAdhX2.jpg',
+  'Bourne Shell': 'http://4.bp.blogspot.com/-ixV6xC69QJA/UAcgvglAv-I/AAAAAAAAAHs/7gqs9M9EVwg/s1600/bash.png',
+  PHP: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/PHP-logo.svg/2000px-PHP-logo.svg.png',
+  Elm: 'http://seeklogo.com/images/E/elm-logo-9DEF2A830B-seeklogo.com.png',
+  XML: 'http://www.bunniktours.com.au/var/plain_site/storage/images/media/images/xml-logo/113656-1-eng-AU/xml-logo.png',
+  Python: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1024px-Python-logo-notext.svg.png',
+  Blade: 'http://kodeinfo.com/img/laravel_logo.png'
+}
+
 Template.hello.helpers({
   totalCloc: function () {
     let repos = DB.Repos.find().fetch()
@@ -51,8 +71,12 @@ Template.hello.helpers({
       types: R.keys(R.omit('SUM', cloc))
     }
 
+
     console.log(totalCloc)
     return totalCloc
+  },
+  logo: function () {
+    return logos[this]
   },
   users: function() {
     return DB.GithubUsers.find();
